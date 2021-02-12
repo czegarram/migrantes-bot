f47a397c0111871b237ab016e0e949e87e110008
Cesar Zegarra <czegarram@gmail.com>
2021-02-12 10:32:31 -0300

    Adding yarn to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "Update app from Git"
 git pull
 
 echo "install packages"
-"$BASE_BIN_FOLDER"yarn install
+"$BASE_BIN_FOLDER"node "$BASE_BIN_FOLDER"yarn install
 
 echo "restart pm2"
 "$BASE_BIN_FOLDER"node "$BASE_BIN_FOLDER"pm2 restart migrantes-bot
