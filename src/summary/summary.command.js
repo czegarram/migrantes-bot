707b62b919d15b7620bebc3a12e803fc46b5fc22
Cesar Zegarra <czegarram@gmail.com>
2021-03-02 22:22:53 -0300

    Update summary.command.js

--- src/summary/summary.command.js
+++ src/summary/summary.command.js
@@ -2,6 +2,6 @@
 
 module.exports = (bot) => {
     bot.command('resumen',  (ctx) => {
-        ctx.reply('Viernes 5 - 7:00pm : Cena en sushi work para despedida Ale&Bryan parte 10000')
+        ctx.reply('Viernes 5 - 7:00pm : Cena en sushi work para despedida Ale&Bryan parte 90')
     })
 }
